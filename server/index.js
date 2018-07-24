ddf82990cc338977bb9b5503536ac7d835240641
Wilkinson, Kevin Mitchell - (willkillson) <willkillson@gmail.com>
2018-07-24 05:10:31 -0700

    poop

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(8080, function(){
-  console.log('listening on *:3000');
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg);
+    });
 });
-    
+http.listen(8080, function () {
+    console.log('listening on *:8080');
+});
