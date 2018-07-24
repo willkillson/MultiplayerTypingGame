2fde9ec11a25e9f8a93de258691427b9e272dce0
kevin wilkinson <willkillson@gmail.com>
2018-07-23 17:27:25 -0700

    Seperated some code from Game.js

--- typer/typer/js/Game.js
+++ typer/typer/js/Game.js
@@ -1,12 +1,5 @@
 ﻿
 
-var inputTextValue = "";//creates a global Javascript variable
-var processText = "";
-
-window.onkeyup = keyup;//creates a listener for when you press a key
-
-
-
 var fire = new Howl({
     src: ['https://s1.vocaroo.com/media/download_temp/Vocaroo_s1wV49PmAmcq.mp3']
 });
@@ -22,42 +15,6 @@ var squish = new Howl({
 });
 
 
-function keyup(e) {
-    
-    if (e.keyCode == 13) {
-        processText = inputTextValue;
-        inputTextValue = "";
-        reload.play();
-        return;
-    }
-
-    if (e.keyCode == 37) {
-        processText = "_PlayerMovement_left";
-        return;
-    }
-    if (e.keyCode == 38) {
-        processText = "_PlayerMovement_up";
-        return;
-    }
-    if (e.keyCode == 39) {
-        processText = "_PlayerMovement_right";
-        return;
-    }
-    if (e.keyCode == 40) {
-        processText = "_PlayerMovement_down";
-        return;
-    }
-
-    if (e.keyCode == 8) {
-        processText = "_PlayerMovement_down";
-        return;
-    }
-
-    fire.play();
-    console.log("Playing fire sound");
-    inputTextValue += String.fromCharCode(e.keyCode);
-    
-}
 
 function Game() {
 
