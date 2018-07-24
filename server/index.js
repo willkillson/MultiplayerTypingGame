9c35593c00244c9f57a600dafe1db1073799c16e
Wilkinson, Kevin Mitchell - (willkillson) <willkillson@gmail.com>
2018-07-24 04:12:36 -0700

    changed port to 8080

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
-http.listen(3000, function(){
+http.listen(8080, function(){
   console.log('listening on *:3000');
 });
     
