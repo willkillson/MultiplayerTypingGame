0dfff338d24c6fff8a05ad9bcf9ce4f191043c45
kevin wilkinson <willkillson@gmail.com>
2018-07-25 15:12:05 -0700

    added movment with left shift

--- typer/typer/js/Keyboard.js
+++ typer/typer/js/Keyboard.js
@@ -1,10 +1,20 @@
 ﻿var inputTextValue = "";//creates a global Javascript variable
 var processText = "";
 window.onkeyup = keyup;//creates a listener for when you press a key
-
 window.onkeydown = keydown;
 
+var isShifted = 0;
+
+
 function keyup(e) {
+
+
+    if (e.keyCode == 16) {//shift modifier
+        isShifted = 0;
+    }
+
+
+
 
     if (e.keyCode == 13) {
         processText = inputTextValue;
@@ -12,23 +22,48 @@ function keyup(e) {
         reload.play();
         return;
     }
+    console.log(e.keyCode);
 
-    if (e.keyCode == 37) {
-        processText = "_PlayerMovement_left";
-        return;
+
+    if (isShifted) {
+
+        if (e.keyCode == 65) {
+            processText = "_PlayerMovement_left";
+            return;
+        }
+        if (e.keyCode == 87) {
+            processText = "_PlayerMovement_up";
+            return;
+        }
+        if (e.keyCode == 68) {
+            processText = "_PlayerMovement_right";
+            return;
+        }
+        if (e.keyCode == 83) {
+            processText = "_PlayerMovement_down";
+            return;
+        }
     }
-    if (e.keyCode == 38) {
-        processText = "_PlayerMovement_up";
-        return;
+    else {
+        if (e.keyCode == 37) {
+            processText = "_PlayerMovement_left";
+            return;
+        }
+        if (e.keyCode == 38) {
+            processText = "_PlayerMovement_up";
+            return;
+        }
+        if (e.keyCode == 39) {
+            processText = "_PlayerMovement_right";
+            return;
+        }
+        if (e.keyCode == 40) {
+            processText = "_PlayerMovement_down";
+            return;
+        }
+
     }
-    if (e.keyCode == 39) {
-        processText = "_PlayerMovement_right";
-        return;
-    }
-    if (e.keyCode == 40) {
-        processText = "_PlayerMovement_down";
-        return;
-    }
+
 
     if (e.keyCode == 8) {
         inputTextValue = inputTextValue.slice(0, inputTextValue.length - 1);
@@ -46,8 +81,9 @@ function keyup(e) {
 
 function keydown(e) {
 
-
-
+    if (e.keyCode == 16) {//shift modifier
+        isShifted = 1;
+    }
 
 
 }
