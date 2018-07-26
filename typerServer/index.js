88eda79387f28d287d20b289757af3be5201d8f4
Wilkinson, Kevin Mitchell - (willkillson) <willkillson@gmail.com>
2018-07-26 09:47:02 -0700

    organized

--- typerServer/index.js
+++ typerServer/index.js
@@ -1,92 +1,16 @@
 
-console.log("Dictionary.js loaded");
 
-//Dictionary
+//Local modules
+const Dictionary = require('./assets/Dictionary');
+const Vec2 = require('./assets/WilkiMath');
+var dictionary = new Dictionary();
 
-var dictionary = new Array();
-
-dictionary.push("grouchy");
-dictionary.push("care");
-dictionary.push("match");
-dictionary.push("weary");
-dictionary.push("snails");
-dictionary.push("market");
-dictionary.push("mature");
-dictionary.push("lethal");
-dictionary.push("abortive");
-dictionary.push("poop");
-dictionary.push("crap");
-dictionary.push("pain");
-dictionary.push("skirt");
-dictionary.push("puzzled");
-dictionary.push("bump");
-dictionary.push("tail");
-dictionary.push("jazzy");
-dictionary.push("stick");
-dictionary.push("endurable");
-dictionary.push("black");
-dictionary.push("rural");
-dictionary.push("faithful");
-dictionary.push("cabbage");
-dictionary.push("crate");
-dictionary.push("talk");
-dictionary.push("guarded");
-dictionary.push("breakable");
-dictionary.push("harmonious");
-dictionary.push("furniture");
-dictionary.push("debonair");
-dictionary.push("thirsty");
-dictionary.push("planes");
-dictionary.push("change");
-dictionary.push("first");
-dictionary.push("bubble");
-dictionary.push("sister");
-dictionary.push("roof");
-dictionary.push("thought");
-dictionary.push("sparkling");
-dictionary.push("adjoining");
-dictionary.push("terrible");
-dictionary.push("launch");
-dictionary.push("comb");
-dictionary.push("foregoing");
-dictionary.push("skinny");
-dictionary.push("ten");
-dictionary.push("rice");
-dictionary.push("girls");
-dictionary.push("productive");
-dictionary.push("poke");
-dictionary.push("guard");
-dictionary.push("mass");
-dictionary.push("jail");
-dictionary.push("disappear");
-dictionary.push("letter");
-dictionary.push("flower");
-dictionary.push("idea");
-dictionary.push("furtive");
-dictionary.push("prickly");
-dictionary.push("capable");
-dictionary.push("seemly");
-dictionary.push("loving");
-
-
-for (let i = 0; i < dictionary.length; i++) {
-    dictionary[i] = dictionary[i].toUpperCase();
-}
-
-
-function Vec2(x, y) {
-    this.x = x;
-    this.y = y;
-}
-//const WilkiMath = require('./assets/WilkiMath');
-
-//const dictionary = require('/assets/Dictionary');
-
+//imported modules
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-
+//routeing
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -136,11 +60,6 @@ io.on('connection', function (socket) {
 
         console.log('a user connected');
     });
-
-    //socket.on('connect', function (msg) {
-    //    console.log('message:' + msg);
-    //});
-
 });
 
 
